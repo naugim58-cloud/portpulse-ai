e55a25c11fa67e129a1d6d3be487618bd8e0b302
naugim58-cloud <310873314+naugim58-cloud@users.noreply.github.com>
2026-08-04 15:02:39 +0900

    Use supplied PortPulse logo artwork

---
+++ logo-image.js
@@ -0,0 +1,1 @@
+const logoMark=document.querySelector('.brand-mark');if(logoMark){logoMark.classList.add('image-logo-mark');logoMark.innerHTML='<img src="assets/portpulse-logo.png" alt="PortPulse 로고">'}
