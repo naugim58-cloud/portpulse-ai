acb62a77a2b8fa38ba4e986c3e736db155dce1a1
naugim58-cloud <310873314+naugim58-cloud@users.noreply.github.com>
2026-08-04 16:30:31 +0900

    Add North Port to logistics mode

---
+++ logistics-port-fix.js
@@ -0,0 +1,3 @@
+logisticsPorts.splice(0,logisticsPorts.length,ports[0],ports[1],ports[2],ports[4]);
+logisticsExtras.splice(0,logisticsExtras.length,portExtras[0],portExtras[1],portExtras[2],portExtras[4]);
+logisticsShips.splice(0,logisticsShips.length,ships[0],ships[1],ships[2],ships[3]);
